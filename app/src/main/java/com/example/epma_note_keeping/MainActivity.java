c5f5f1200b1eb03b6447e4f94adf7aaab8ffc53a
Matias S. Baiges <mbaiges@itba.edu.ar>
2023-03-12 12:12:09 +0100

    feat: base android app

---
+++ app/src/main/java/com/example/epma_note_keeping/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.epma_note_keeping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
